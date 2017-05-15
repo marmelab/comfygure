0613d8edb96102257c6e0a00fe0a8f9cafb624ac
ThieryMichel <thiery@marmelab.com>
2017-05-15 13:07:23 +0200

    remove useless dependencies

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,8 +1,5 @@
 import React from 'react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import { Card, CardActions, CardHeader, CardTitle, CardText } from 'material-ui/Card';
-import TextField from 'material-ui/TextField';
-import RaisedButton from 'material-ui/RaisedButton';
 
 import Login from './Login';
 
