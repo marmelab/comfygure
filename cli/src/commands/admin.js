e173f6a43dd6685e1216ce55551ed3f3062f4b82
ThieryMichel <thiery@marmelab.com>
2017-05-15 16:53:41 +0200

    have `comfy admin` run comfy-admin or propose to install it if not present

--- cli/src/commands/admin.js
+++ cli/src/commands/admin.js
@@ -1,16 +1,49 @@
-const HttpServer = require('http-server');
+const exec = require('child_process').exec;
 
-module.exports = ui => () =>
+function moduleAvailable(name) {
+    try {
+        require.resolve(name);
+        return true;
+    } catch (e) {} // eslint-disable-line no-empty
+    return false;
+}
+
+const runCommand = cmd =>
     new Promise((resolve, reject) => {
-        const server = HttpServer.createServer({ root: './admin' });
-        server.listen(3000, (error) => {
+        exec(cmd, (error) => {
             if (error) {
                 reject(error);
+                return;
             }
-            ui.print(`
-    launching admin on http://localhost:3000
-    press Ctrl+C to end`
-            );
+
             resolve();
         });
     });
+
+module.exports = ui => function* () {
+    if (moduleAvailable('comfy-admin')) {
+        yield runCommand('comfy-admin');
+        return;
+    }
+    ui.print('You need to install comfy-admin: npm install -g comfy-admin');
+    const response = yield ui.input.text('Do you want us to install it for you ? y/n');
+
+    if (response.toLowerCase() !== 'y') {
+        return;
+    }
+
+    try {
+        yield runCommand('npm install -g comfy');
+    } catch (error) {
+        if (error.message.match('Please try running this command again as root/Administrator.')) {
+            ui.print(`
+    Uh oh, it looks like npm need administrator rights to install package globally on your machine.
+    Either run the command with sudo and trust us or look on internet to see how to configure your
+    environment so that sudo is no longer required to install global packages (which is a lot better)
+            `);
+            return;
+        }
+        throw error;
+    }
+    yield runCommand('comfy-admin');
+};
