2af79142f4978678c5d5b6489feb486404bb0a00
gildas <gildas.garcia@gmail.com>
2017-05-15 17:19:38 +0200

    missing files & styles

---
+++ admin/src/Tags.js
@@ -0,0 +1,61 @@
+import React, { Component } from 'react';
+import PropTypes from 'proptypes';
+import { Toolbar, ToolbarGroup, ToolbarSeparator, ToolbarTitle } from 'material-ui/Toolbar';
+import DropDownMenu from 'material-ui/DropDownMenu';
+import MenuItem from 'material-ui/MenuItem';
+import NavigationExpandMoreIcon from 'material-ui/svg-icons/navigation/expand-more';
+
+const styles = {
+    tag: {
+        lineHeight: '48px',
+        fontSize: '20px',
+        fontFamily: 'Roboto, sans-serif',
+        position: 'relative',
+        textOverflow: 'ellipsis',
+        whiteSpace: 'nowrap',
+        overflow: 'hidden',
+        color: 'rgba(0, 0, 0, 0.4)',
+    },
+    icon: {
+        fill: 'rgba(0, 0, 0, 0.4)',
+        marginTop: '-3px',
+    },
+};
+
+class Tags extends Component {
+    handleChange = (event, index, value) => {
+        this.props.onTagSelected(value);
+    }
+
+    render() {
+        const { environment, tag: currentTag, tags } = this.props;
+
+        return (
+            <Toolbar>
+                <ToolbarGroup>
+                    <ToolbarTitle text={environment.name} />
+                    <ToolbarSeparator />
+                    <DropDownMenu
+                        value={currentTag.name}
+                        onChange={this.handleChange}
+                        labelStyle={styles.tag}
+                        iconStyle={styles.icon}
+                    >
+                        {tags.map(tag => (
+                            <MenuItem key={tag.name} value={tag.name} primaryText={tag.name} />
+                        ))}
+                    </DropDownMenu>
+                </ToolbarGroup>
+            </Toolbar>
+        );
+    }
+}
+
+Tags.propTypes = {
+    environment: PropTypes.object,
+    onTagSelected: PropTypes.func.isRequired,
+    tag: PropTypes.object,
+    tags: PropTypes.arrayOf(PropTypes.object),
+}
+
+export default Tags;
