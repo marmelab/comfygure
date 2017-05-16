1e5fd6d15f983a3fa4435f09f14aea6a1f8e19df
ThieryMichel <thiery@marmelab.com>
2017-05-16 14:25:26 +0200

    fetch environments on submit

---
+++ admin/src/utils/handleFetchResponse.js
@@ -0,0 +1,22 @@
+export default response => {
+    if (response.status === 204) {
+        return null;
+    }
+    if (response.status >= 200 && response.status < 300) {
+        return response.json();
+    }
+
+    return response
+        .json()
+        .then((json) => {
+            const error = new Error(json.error);
+            error.response = response;
+            error.code = response.status;
+            throw error;
+        }, () => {
+            const error = new Error(response.statusText);
+            error.response = response;
+            error.code = response.status;
+            throw error;
+        });
+};
