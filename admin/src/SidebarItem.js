aabfb39bcd2c6b3501238b532d52d1171a10cadb
gildas <gildas.garcia@gmail.com>
2017-05-16 09:45:31 +0200

    Cleanup and lint

--- admin/src/SidebarItem.js
+++ admin/src/SidebarItem.js
@@ -1,18 +1,19 @@
 import React, { Component } from 'react';
+import PropTypes from 'proptypes';
 import MenuItem from 'material-ui/MenuItem';
 
 export default class SidebarItem extends Component {
+    static propTypes = {
+        onSelected: PropTypes.func.isRequired,
+        text: PropTypes.string.isRequired,
+        value: PropTypes.string.isRequired,
+    };
+
     handleClick = () => {
         this.props.onSelected(this.props.value);
-    }
+    };
     render() {
         const { text } = this.props;
-        return (
-            <MenuItem
-                primaryText={text}
-                onTouchTap={this.handleClick}
-            />
-        );
+        return <MenuItem primaryText={text} onTouchTap={this.handleClick} />;
     }
 }
-
