75a6d487682a31a0670c90bb1c7732ca49a25a36
ThieryMichel <thiery@marmelab.com>
2017-05-17 10:29:34 +0200

    add fetch helper and test submit effet

--- admin/src/provideAppState.js
+++ admin/src/provideAppState.js
@@ -2,18 +2,15 @@ import { provideState, softUpdate } from 'freactal';
 
 export const state = {
     initialState: () => ({
+        projectId: '',
         token: '',
         secret: '',
         environments: null,
     }),
     effects: {
-        setProjectId: softUpdate((state, projectId) => ({
+        setConfig: softUpdate((state, { projectId, token, secret }) => ({
             projectId,
-        })),
-        setToken: softUpdate((state, token) => ({
             token,
-        })),
-        setSecret: softUpdate((state, secret) => ({
             secret,
         })),
         setEnvironments: softUpdate((state, environments) => ({
