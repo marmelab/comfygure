7c59a78477f57e815e7dd8421ebb73e522457d13
gildas <gildas.garcia@gmail.com>
2017-05-22 15:04:32 +0200

    [WIP] Config edition
    - [x] Updated config view
    - [x] Added edition view
    - [ ] Tests

--- admin/src/Environment.js
+++ admin/src/Environment.js
@@ -1,8 +1,14 @@
 import React, { Component } from 'react';
 import PropTypes from 'proptypes';
-import JsonView from 'react-pretty-json';
+import JSONTree from 'react-json-tree';
+import AceEditor from 'react-ace';
+import 'brace/mode/javascript';
+import 'brace/theme/github';
+
 import { injectState } from 'freactal';
 import LinearProgress from 'material-ui/LinearProgress';
+import { Toolbar, ToolbarGroup } from 'material-ui/Toolbar';
+import RaisedButton from 'material-ui/RaisedButton';
 
 import provideConfigState from './provideConfigState';
 import Alert from './components/Alert';
@@ -14,6 +20,35 @@ const styles = {
         flexGrow: 2,
         padding: '1em',
     },
+    config: {
+        paddingTop: '1em',
+    },
+};
+
+const treeTheme = {
+    scheme: 'google',
+    author: 'seth wright (http://sethawright.com)',
+    base00: '#1d1f21',
+    base01: '#282a2e',
+    base02: '#373b41',
+    base03: '#969896',
+    base04: '#b4b7b4',
+    base05: '#c5c8c6',
+    base06: '#e0e0e0',
+    base07: '#ffffff',
+    base08: '#CC342B',
+    base09: '#F96A38',
+    base0A: '#FBA922',
+    base0B: '#198844',
+    base0C: '#3971ED',
+    base0D: '#3971ED',
+    base0E: '#A36AC7',
+    base0F: '#3971ED',
+};
+
+const aceOptions = {
+    showLineNumbers: true,
+    tabSize: 4,
 };
 
 class Environment extends Component {
@@ -27,14 +62,46 @@ class Environment extends Component {
         }
     }
 
+    handleSaveClick = () => {
+        this.props.saveConfig(this.props.newConfig);
+    };
+
     render() {
-        const { config, error, loading } = this.props;
+        const { config, edition, error, loading, newConfig, setNewConfig, toggleEdition } = this.props;
 
         return (
             <div style={styles.container}>
+                <Toolbar>
+                    <ToolbarGroup firstChild>
+                        {!edition && <RaisedButton label="Edit" primary onClick={toggleEdition} />}
+                        {edition && <RaisedButton label="Save" primary onClick={this.handleSaveClick} />}
+                        {edition && <RaisedButton label="Cancel" onClick={toggleEdition} />}
+                    </ToolbarGroup>
+                </Toolbar>
                 {loading && <LinearProgress mode="indeterminate" />}
                 {error && <Alert message={error} />}
-                {!loading && config && <JsonView json={config} spaces={4} />}
+                {!loading &&
+                    config &&
+                    !edition &&
+                    <div style={styles.config}>
+                        <JSONTree hideRoot theme={treeTheme} data={config} />
+                    </div>}
+                {!loading &&
+                    config &&
+                    edition &&
+                    <div style={styles.config}>
+                        <AceEditor
+                            mode="javascript"
+                            theme="github"
+                            name="configEditor"
+                            showGutter
+                            highlightActiveLine
+                            value={JSON.stringify(newConfig, null, 4)}
+                            setOptions={aceOptions}
+                            onChange={setNewConfig}
+                            width="100%"
+                        />
+                    </div>}
             </div>
         );
     }
@@ -42,10 +109,15 @@ class Environment extends Component {
 
 Environment.propTypes = {
     config: PropTypes.object,
+    edition: PropTypes.bool.isRequired,
     environmentName: PropTypes.string,
     error: PropTypes.string,
     getConfig: PropTypes.func.isRequired,
     loading: PropTypes.bool,
+    newConfig: PropTypes.object,
+    saveConfig: PropTypes.func.isRequired,
+    setNewConfig: PropTypes.func.isRequired,
+    toggleEdition: PropTypes.func.isRequired,
 };
 
 Environment.defaultProps = {
@@ -54,13 +126,21 @@ Environment.defaultProps = {
 
 export default provideConfigState(
     injectState(
-        ({ state: { config, environmentName, error, loading, projectId, secret, token }, effects: { getConfig } }) => (
+        ({
+            state: { config, edition, environmentName, error, loading, newConfig, projectId, secret, token },
+            effects: { getConfig, saveConfig, setNewConfig, toggleEdition },
+        }) => (
             <Environment
-                environmentName={environmentName}
                 config={config}
+                edition={edition}
+                environmentName={environmentName}
                 error={error}
-                loading={loading}
                 getConfig={environmentName => getConfig({ environmentName, projectId, secret, token })}
+                loading={loading}
+                newConfig={newConfig}
+                saveConfig={config => saveConfig({ config, environmentName, projectId, secret, token })}
+                setNewConfig={setNewConfig}
+                toggleEdition={toggleEdition}
             />
         ),
     ),
--- admin/src/provideConfigState.js
+++ admin/src/provideConfigState.js
@@ -6,7 +6,8 @@ import fetchState from './fetch/state';
 import wrapWithLoading from './utils/wrapWithLoading';
 import wrapWithErrorHandling from './utils/wrapWithErrorHandling';
 import fetchConfig from './fetch/fetchConfig';
-import { decrypt } from './utils/crypto';
+import updateConfig from './fetch/updateConfig';
+import { decrypt, encrypt } from './utils/crypto';
 
 // `null` and `undefined` are the only types that we cannot stringify
 const isNullValue = value => value === null || value === undefined;
@@ -19,6 +20,20 @@ export const decryptConfig = (config, secret) =>
             return {
                 ...acc,
                 [key]: decrypt(value.toString(), secret),
+            };
+        }
+
+        return acc;
+    }, {});
+
+export const encryptConfig = (config, secret) =>
+    Object.keys(config).reduce((acc, key) => {
+        const value = config[key];
+
+        if (!isNullValue(value)) {
+            return {
+                ...acc,
+                [key]: encrypt(value.toString(), secret),
             };
         }
 
@@ -39,17 +54,29 @@ export const getConfigSaga = function*(effects, { secret, ...args }) {
     }
 };
 
+export const updateConfigSaga = function*(effects, { secret, config, ...args }) {
+    yield call(effects.setLoading, true);
+    const encryptedConfig = yield call(encryptConfig, config, secret);
+    yield call(updateConfig, { ...args, config: encryptedConfig });
+    yield call(effects.setConfig, encryptedConfig);
+    yield call(effects.setLoading, false);
+};
+
 export const state = {
     initialState: () => ({
         ...fetchState.state,
         config: undefined,
+        edition: false,
         error: undefined,
         loading: false,
     }),
     effects: {
         ...fetchState.effects,
-        setConfig: softUpdate((state, config) => ({ config })),
+        setConfig: softUpdate((state, config) => ({ config, newConfig: config })),
+        toggleEdition: softUpdate(({ edition }) => ({ edition: !edition })),
         getConfig: wrapWithErrorHandling(wrapWithLoading((effects, args) => sg(getConfigSaga)(effects, args))),
+        saveConfig: wrapWithErrorHandling(wrapWithLoading((effects, args) => sg(updateConfigSaga)(effects, args))),
+        setNewConfig: softUpdate((state, newConfig) => ({ newConfig: JSON.parse(newConfig) })),
     },
 };
 
