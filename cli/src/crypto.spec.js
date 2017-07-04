fc39e4a1ca541cea871b2fe593f7a56c26308fe2
Kmaschta <kevin@marmelab.com>
2017-07-04 09:33:12 +0200

    Add a test for a better crypto

--- cli/src/crypto.spec.js
+++ cli/src/crypto.spec.js
@@ -14,4 +14,21 @@ describe('Crypto Features', () => {
 
         expect(decryptedData).toBe(data);
     });
+
+    it('should not return the identic signature twice for the same given entry and passphrase', () => {
+        const data = 'SOME VERY PRIVATE INFO';
+        const passphrase = 'passphrase';
+        const algo = 'aes-256-ctr';
+
+        const encryptedData = encrypt(data, passphrase, algo);
+        const encryptedData2 = encrypt(data, passphrase, algo);
+
+        expect(encryptedData).toNotBe(encryptedData2);
+
+        const decryptedData = decrypt(encryptedData, passphrase);
+        const decryptedData2 = decrypt(encryptedData2, passphrase);
+
+        expect(decryptedData).toBe(data);
+        expect(decryptedData2).toBe(data);
+    });
 });
