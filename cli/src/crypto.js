24b6c67ba541e4e33e7acc8118388972cb8e7149
Kmaschta <kevin@marmelab.com>
2017-08-02 17:37:37 +0200

    Code Review

--- cli/src/crypto.js
+++ cli/src/crypto.js
@@ -7,30 +7,34 @@ const IV_LENGTH = 16;
 const hexToBuffer = hex => new Buffer(hex, 'hex');
 const bufferToHex = buffer => buffer.toString('hex');
 
-const bufferize = (key, tryToCast = true) => {
-    if (!Buffer.isBuffer(key) || key.length !== KEY_BYTE_LENGTH) {
-        if (tryToCast) {
-            return bufferize(hexToBuffer(key), false);
+const castKeyToBuffer = (key, castToBuffer = true) => {
+    if (Buffer.isBuffer(key)) {
+        if (key.length === KEY_BYTE_LENGTH) {
+            return key;
         }
 
-        throw new Error(`The "key" parameter should be a ${KEY_BYTE_LENGTH} bytes Buffer`);
+        throw new Error(`The "key" argument must be a ${KEY_BYTE_LENGTH} bytes Buffer`);
     }
 
-    return key;
+    if (castToBuffer) {
+        return castKeyToBuffer(hexToBuffer(key), false);
+    }
+
+    throw new Error('The "key" argument is must be a Buffer or a hexadecimal-encoded string');
 };
 
 const encrypt = (value, hexKey) => {
-    const key = bufferize(hexKey);
+    const key = castKeyToBuffer(hexKey);
     const iv = crypto.randomBytes(IV_LENGTH);
 
     const cipher = crypto.createCipheriv(ALGORITHM, key, iv);
-    const cipherText = cipher.update(value, 'utf-8', 'hex') + cipher.final('hex');
+    const cipherText = Buffer.concat([cipher.update(value, 'utf-8'), cipher.final()]);
 
-    return `${ALGORITHM}:${iv.toString('hex')}:${cipherText}`;
+    return `${ALGORITHM}:${iv.toString('hex')}:${cipherText.toString('hex')}`;
 };
 
 const decrypt = (entry, hexKey) => {
-    const key = bufferize(hexKey);
+    const key = castKeyToBuffer(hexKey);
 
     const [algorithm, hexIV, value] = entry.split(':');
 
@@ -40,8 +44,9 @@ const decrypt = (entry, hexKey) => {
 
     const iv = hexToBuffer(hexIV);
     const decipher = crypto.createDecipheriv(ALGORITHM, key, iv);
+    const decipherText = Buffer.concat([decipher.update(value, 'hex'), decipher.final()]);
 
-    return decipher.update(value, 'hex', 'utf-8') + decipher.final('utf-8');
+    return decipherText.toString('utf-8');
 };
 
 const generateNewPrivateKey = () => bufferToHex(crypto.randomBytes(KEY_BYTE_LENGTH));
--- cli/src/crypto.spec.js
+++ cli/src/crypto.spec.js
@@ -1,11 +1,10 @@
 const expect = require('expect');
-const crypto = require('crypto');
-const { encrypt, decrypt, KEY_BYTE_LENGTH } = require('./crypto');
+const { encrypt, decrypt, generateNewPrivateKey } = require('./crypto');
 
 describe('Crypto Features', () => {
     it('should keep the consistancy between encryption & decryption', () => {
         const data = 'SOME VERY PRIVATE INFO';
-        const privateKey = crypto.randomBytes(KEY_BYTE_LENGTH);
+        const privateKey = generateNewPrivateKey();
         const encryptedData = encrypt(data, privateKey);
 
         expect(encryptedData).toNotBe(data);
@@ -17,7 +16,7 @@ describe('Crypto Features', () => {
 
     it('should not return the identic signature twice for the same given entry and private key', () => {
         const data = 'SOME VERY PRIVATE INFO';
-        const privateKey = crypto.randomBytes(KEY_BYTE_LENGTH);
+        const privateKey = generateNewPrivateKey();
 
         const encryptedData = encrypt(data, privateKey);
         const encryptedData2 = encrypt(data, privateKey);
