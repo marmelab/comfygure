49b01060ce42212a2aa0b09ba6f8bb7687cb71b4
Kmaschta <kevin@marmelab.com>
2017-09-18 10:15:52 +0200

    format: handle null value with envvars

---
+++ cli/src/format/index.spec.js
@@ -0,0 +1,12 @@
+const expect = require('expect');
+const { toEnvVars } = require('./');
+
+describe('Format', () => {
+    describe('toEnvVars', () => {
+        it('should handle null value', () => {
+            expect(toEnvVars({ key: '' })).toEqual("export KEY='';\n");
+            expect(toEnvVars({ key: null })).toEqual("export KEY='';\n");
+            expect(toEnvVars({ key: undefined })).toEqual("export KEY='';\n");
+        });
+    });
+});
