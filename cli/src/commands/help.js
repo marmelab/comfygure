719fc44fca333ccacb7109a994b72b94e9e8a997
Kmaschta <kevin@marmelab.com>
2017-09-18 10:29:39 +0200

    Version 0.1.1

--- cli/src/commands/help.js
+++ cli/src/commands/help.js
@@ -16,13 +16,12 @@ ${bold('COMMANDS')}
         env         Manage configuration environments
         tag         Manage configuration tags
         log         List all configuration versions
-        admin       Launch the admin interface
 
 ${bold('EXAMPLES')}
         ${dim('# Display the help')}
         ${cyan('comfy help')}
         ${dim('# Initialize comfy in the current directory')}
-        ${cyan(`comfy init --origin 'http://mycomfy.mydomain.com'`)}
+        ${cyan("comfy init --origin 'http://mycomfy.mydomain.com'")}
         ${dim('# Set a new configuration version')}
         ${cyan('comfy setall development config/api.json')}
         ${dim('# Add production environment')}
@@ -33,8 +32,6 @@ ${bold('EXAMPLES')}
         ${cyan('comfy log production')}
         ${dim('# Retrieve the latest development configuration and use it to set env vars')}
         ${cyan('comfy get development --envvars | source /dev/stdin')}
-        ${dim('# Launch the web admin on port 8080')}
-        ${cyan('comfy admin -p 8080')}
 
 ${bold('ABOUT')}
         ${bold('comfy')} is licensed under the MIT Licence, sponsored and supported by marmelab.
