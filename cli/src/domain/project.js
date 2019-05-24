50b6cdeec91f6e728f4d51ae579a3b02c0b0d1af
Kmaschta <kevin@marmelab.com>
2019-05-24 18:30:46 +0200

    Store encoded credentials in a single environment variable

--- cli/src/domain/project.js
+++ cli/src/domain/project.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const ini = require('ini');
 const path = require('path');
-const { CONFIG_FOLDER, CONFIG_PATH, DEFAULT_ORIGIN } = require('./constants');
+const { CONFIG_FOLDER, CONFIG_PATH, DEFAULT_ORIGIN, CREDENTIALS_VARIABLE } = require('./constants');
 const { generateNewPrivateKey, generateNewHmacKey } = require('../crypto');
 
 const getConfigFolder = () => `${process.cwd()}${path.sep}${CONFIG_FOLDER}`;
@@ -50,50 +50,56 @@ module.exports = (client, ui) => {
 
     const checkProjectInfos = ({ id, accessKey, secretToken, privateKey, hmacKey, origin }) => {
         const errors = [];
-        const { red, dim, bold } = ui.colors;
-
-        const set = v => `Please set ${dim(v)} or edit your ${dim(CONFIG_PATH)} file.`;
+        const { red, bold } = ui.colors;
 
         if (!id) {
-            errors.push(`Unable to locate the ${red('project identifier')}.
-${set('COMFY_PROJECT_ID')}`);
+            errors.push(`Unable to locate the ${red('project identifier')}.`);
         }
 
         if (!accessKey) {
-            errors.push(`Unable to locate the ${red('access key')}.
-${set('COMFY_ACCESS_KEY')}`);
+            errors.push(`Unable to locate the ${red('access key')}.`);
         }
 
         if (!secretToken) {
-            errors.push(`Unable to locate the ${red('secret token')}.
-${set('COMFY_SECRET_TOKEN')}`);
+            errors.push(`Unable to locate the ${red('secret token')}.`);
         }
 
         if (!privateKey) {
-            errors.push(`Unable to locate the ${red('private key')} to decrypt your configs.
-${set('COMFY_PRIVATE_KEY')}`);
+            errors.push(`Unable to locate the ${red('private key')} to decrypt your configs.`);
         }
 
         if (!hmacKey) {
-            errors.push(`Unable to locate the ${red('hmac key')} to sign and verify your configs.
-${set('COMFY_HMAC_KEY')}`);
+            errors.push(`Unable to locate the ${red('hmac key')} to sign and verify your configs.`);
         }
 
         if (!origin) {
-            errors.push(`Unable to locate the ${red('server origin')} to decrypt your configs.
-${set('COMFY_ORIGIN')}`);
+            errors.push(`Unable to locate the ${red('server origin')} to decrypt your configs.`);
         }
 
         if (errors.length > 0) {
             ui.error(`${errors.join('\n')}
 
-Have you tried to initialize comfy on this folder?
+Have you exported the ${bold(CREDENTIALS_VARIABLE)} environment variable?
+
+Have you tried to initialize comfy in this folder?
 Type ${bold('comfy init')} to do so.`);
             ui.exit(1);
         }
     };
 
     const retrieveFromConfig = () => {
+        if (process.env[CREDENTIALS_VARIABLE]) {
+            try {
+                const buffer = Buffer.from(process.env[CREDENTIALS_VARIABLE], 'base64');
+                const credentials = JSON.parse(buffer.toString('utf8'));
+                checkProjectInfos(credentials);
+                return credentials;
+            } catch (error) {
+                ui.error(`The credentials encoded in ${CREDENTIALS_VARIABLE} are invalid`);
+                ui.exit(1);
+            }
+        }
+
         const envs = {
             id: process.env.COMFY_PROJECT_ID,
             accessKey: process.env.COMFY_ACCESS_KEY,
@@ -109,7 +115,8 @@ Type ${bold('comfy init')} to do so.`);
             checkProjectInfos(envs);
             return envs;
         }
-        const file = fs.readFileSync(filename, 'utf-8');
+
+        const file = fs.readFileSync(filename, 'utf8');
         const config = ini.parse(file);
 
         const projectInfos = Object.assign({}, envs, {
@@ -123,6 +130,12 @@ Type ${bold('comfy init')} to do so.`);
 
         checkProjectInfos(projectInfos);
         return projectInfos;
+    };
+
+    const toEncodedCredentials = () => {
+        const project = retrieveFromConfig();
+        const buffer = Buffer.from(JSON.stringify(project), 'utf8');
+        return buffer.toString('base64');
     };
 
     const permanentlyDelete = function*() {
@@ -146,10 +159,12 @@ Type ${bold('comfy init')} to do so.`);
         saveToConfig,
         CONFIG_FOLDER,
         CONFIG_PATH,
+        CREDENTIALS_VARIABLE,
         generateNewPrivateKey,
         generateNewHmacKey,
         getConfigFolder,
         getConfigPath,
         permanentlyDelete,
+        toEncodedCredentials,
     };
 };
