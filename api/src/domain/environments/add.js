fffeb1c840bf478acd87904dc23859f19168bd18
Kmaschta <kevin@marmelab.com>
2020-03-11 16:08:25 +0100

    Replace configuration queries

--- api/src/domain/environments/add.js
+++ api/src/domain/environments/add.js
@@ -1,39 +1,39 @@
-import hash from 'object-hash';
+import hash from "object-hash";
 
-import environmentsQueries from '../../queries/environments';
-import configurationsQueries from '../../queries/configurations';
-import versionsQueries from '../../queries/versions';
-import { add as addTag } from '../configurations/tag';
+import environmentsQueries from "../../queries/environments";
+import configurationsQueries from "../../queries/configurations";
+import versionsQueries from "../../queries/versions";
+import { add as addTag } from "../configurations/tag";
 
-import { LIVE } from '../common/states';
-import { ENVVARS } from '../common/formats';
+import { LIVE } from "../common/states";
+import { ENVVARS } from "../common/formats";
 
-export default async (projectId, environmentName, configName = 'default') => {
-    // TODO (Kevin): Check if the environment already exists and return a usable error if it's the case
-    // TODO (Kevin): Factorize the code with domain/configurations/add
+export default async (projectId, environmentName, configName = "default") => {
+  // TODO (Kevin): Check if the environment already exists and return a usable error if it's the case
+  // TODO (Kevin): Factorize the code with domain/configurations/add
 
-    const environment = await environmentsQueries.insertOne({
-        name: environmentName,
-        project_id: projectId,
-        state: LIVE
-    });
+  const environment = await environmentsQueries.insertOne({
+    name: environmentName,
+    project_id: projectId,
+    state: LIVE
+  });
 
-    const configuration = await configurationsQueries.insertOne({
-        environment_id: environment.id,
-        name: configName,
-        default_format: ENVVARS
-    });
+  const configuration = await configurationsQueries.insertOne({
+    environment_id: environment.id,
+    name: configName,
+    default_format: ENVVARS
+  });
 
-    const version = await versionsQueries.insertOne({
-        configuration_id: configuration.id,
-        hash: hash({ previous: null }),
-        previous: null
-    });
+  const version = await versionsQueries.insertOne({
+    configuration_id: configuration.id,
+    hash: hash({ previous: null }),
+    previous: null
+  });
 
-    await addTag(configuration.id, version.id, 'latest');
+  await addTag(configuration.id, version.id, "latest");
 
-    return {
-        ...environment,
-        configurations: [configuration]
-    };
+  return {
+    ...environment,
+    configurations: [configuration]
+  };
 };
--- api/src/queries/configurations.js
+++ api/src/queries/configurations.js
@@ -1,88 +1,82 @@
-import { crudQueries } from 'co-postgres-queries';
+import omit from "lodash.omit";
+import client, {
+  insertOne as insertOneQuery,
+  updateOne as updateOneQuery
+} from "./knex";
 
-import db from './db';
-
-const table = 'configuration';
+const table = "configuration";
 const fields = [
-    'id',
-    'environment_id',
-    'name',
-    'state',
-    'default_format',
-    'created_at',
-    'updated_at',
+  "configuration.id",
+  "configuration.name",
+  "configuration.state",
+  "configuration.environment_id",
+  "configuration.default_format",
+  "configuration.created_at",
+  "configuration.updated_at",
+  "environment.name as environment_name",
+  "environment.project_id as project_id"
 ];
-const idFields = ['id'];
-const returnFields = ['id', 'name', 'state', 'default_format'];
-
-const query = crudQueries(table, fields, idFields, returnFields);
-
-query.selectPage = query.selectPage
-    .table(`
-        project
-        LEFT JOIN environment ON (project.id = environment.project_id)
-        LEFT JOIN configuration ON (environment.id = configuration.environment_id)
-    `)
-    .searchableFields(['project_id', 'environment.name', 'configuration.name'])
-    .idFields(['configuration.id'])
-    .returnFields([
-        'configuration.id',
-        'configuration.environment_id',
-        'configuration.name',
-        'configuration.state',
-        'configuration.default_format',
-        'configuration.created_at',
-        'configuration.updated_at',
-        'environment.name as environment_name',
-        'project.id as project_id',
-    ]);
 
 const findOne = async (projectId, environmentName, configurationName) => {
-    const client = await db.link(query);
-    const result = await client.selectPage(undefined, undefined, {
-        project_id: projectId,
-        'environment.name': environmentName,
-        'configuration.name': configurationName,
-        state: 'live',
+  const results = await client
+    .select(fields)
+    .from(table)
+    .innerJoin("environment", "environment.id", "configuration.environment_id")
+    .where({
+      project_id: projectId,
+      "environment.name": environmentName,
+      "configuration.name": configurationName,
+      "configuration.state": "live"
     });
-    client.release();
 
-    if (!result.length) {
-        return null;
-    }
-    return result[0];
+  return results[0];
 };
 
-const findAllByEnvironmentName = async (projectId, environmentName) => {
-    const client = await db.link(query);
-    const result = await client.selectPage(undefined, undefined, {
-        project_id: projectId,
-        'environment.name': environmentName,
-        state: 'live',
+const findAllByEnvironmentName = async (projectId, environmentName) =>
+  client
+    .select(fields)
+    .from(table)
+    .innerJoin("environment", "environment.id", "configuration.environment_id")
+    .where({
+      project_id: projectId,
+      "environment.name": environmentName,
+      "configuration.state": "live"
     });
-    client.release();
 
-    return result;
-};
+const insertOne = async data => {
+  const { id } = await insertOneQuery(table, ["id"])(data);
 
-const insertOne = async (configurationData) => {
-    const client = await db.link(query);
-    const configuration = await client.insertOne(configurationData);
-    client.release();
-    return configuration;
+  const results = await client
+    .select(fields)
+    .from(table)
+    .innerJoin("environment", "environment.id", "configuration.environment_id")
+    .where({
+      "configuration.id": id
+    });
+
+  return results[0];
 };
 
 const updateOne = async (id, data) => {
-    const client = await db.link(query);
-    const configuration = client.updateOne(id, data);
-    client.release();
+  await updateOneQuery(table, ["id"])(
+    id,
+    omit(data, ["project_id", "environment_name"])
+  );
 
-    return configuration;
+  const results = await client
+    .select(fields)
+    .from(table)
+    .innerJoin("environment", "environment.id", "configuration.environment_id")
+    .where({
+      "configuration.id": id
+    });
+
+  return results[0];
 };
 
 export default {
-    findOne,
-    findAllByEnvironmentName,
-    insertOne,
-    updateOne,
+  findOne,
+  findAllByEnvironmentName,
+  insertOne,
+  updateOne
 };
